3e5e513ceaf186dab7ee7e1ddd63a948799486b3
Théo Segonds <theo.segonds@inria.fr>
2015-06-26 16:03:26 +0200

    setup.py !

---
+++ gmm_lbd/__init__.py
@@ -0,0 +1,1 @@
+from lbd import Sanitize_records_for_gmm
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="gmm-lbd",
+    version="0.0.1",
+    description="Some gmm regression experiments for learning by demonstration",
+    license="Beer Ware",
+    author="Théo Segonds",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
